2f06155902a3163d15b2507f20a37fce766eea9e
LI_MAX <3096408586@qq.com>
2023-05-21 20:21:28 +0800

    5.21_20.20add position and speed

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,16 +12,32 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
         #self.bg_color = (230,230,230)
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        #绘制所要求的平面
+        self.screen.fill(self.setting.bg_color)
+        self.ship.blitme()
+        #返回机制
+        pygame.display.flip()#对屏幕进行隐藏
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            #绘制所要求的平面
-            self.screen.fill(self.setting.bg_color)
-            self.ship.blitme()
-            #返回机制
-            pygame.display.flip()#对屏幕进行隐藏
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
--- ship.py
+++ ship.py
@@ -9,6 +9,15 @@ class Ship:
         self.rect = self.image.get_rect()
         #新飞机放在屏幕底部的中央
         self.rect.midbottom = self.screen_rect.midbottom
+        #移动标志
+        self.moving_right = False
+        self.moving_left = False
+        self.ship_speed = 1.5
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1
+        elif self.moving_left:
+            self.rect.y +=1
     def blitme(self):
         '''指定位置绘制飞机'''
         self.screen.blit(self.image,self.rect)
